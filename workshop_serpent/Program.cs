2ec7cab41bb1c2506bbf9abe8aceaa6b7d3ba8ba
Cyrilloche <cyril.cherrier.dev@gmail.com>
2024-02-01 21:20:59 +0100

    ⚡Ajout d'une liste de joueur - Ajout de la fonction Move() - Modification des fonctions suivants modifications

--- workshop_serpent/Program.cs
+++ workshop_serpent/Program.cs
@@ -4,46 +4,33 @@
     {
         static void Main(string[] args)
         {
-            /* Créer une liste de joueur, avec un nom, et un carctère à définir
-             * dans la fonction Display, permettre d'afficher les plateaux des joueurs peu importe leur nombre
-             * modifier les autres fonctions pour qu'elle s'applique peu importe le nombre de joueur */
+            /*TO DO*/
+            /* Créer une fonction qui ajoute dynamiquement un nombre de joueur en fonction du nombres de nom entrés */
+            /* Refaire le fonction display*/
 
-            int positionPlayer1 = 0;
-            char player1 = '@';
-            char player2 = '#';
-            int positionPlayer2 = 0;
+            //char player1 = '@';
+            //char player2 = '#';
             int positionEnd = 50;
-            int resultDe;
 
+            List<string> listOfPlayers = new List<string>();
+            listOfPlayers.Add("Leila");
+            listOfPlayers.Add("Cyril");
+
+            int[] positionOfPlayers = new int[listOfPlayers.Count];
             bool play = true;
 
             Console.WriteLine();
 
             while (play)
             {
-                /* faire une méthode pour éviter la duplication de codes*/
-                Display(positionPlayer1, positionPlayer2, player1, player2);
-                resultDe = LancerDeDes();
-                Console.WriteLine("Le joueur 1 a obtenu un {0}", resultDe);
-
-                positionPlayer1 += resultDe;
-
-                resultDe = LancerDeDes();
-                Console.WriteLine("Le joueur 2 a obtenu un {0}", resultDe);
-
-                positionPlayer2 += resultDe;
-
-                play = IsFinish(positionPlayer1, positionPlayer2, positionEnd);
-
-                positionPlayer1 = FallInTrap(positionPlayer1);
-                positionPlayer2 = FallInTrap(positionPlayer2);
-
-                positionPlayer1 = Bonus(positionPlayer1);
-                positionPlayer2 = Bonus(positionPlayer2);
-
-
-                Console.WriteLine("Position du joueur 1: {0}", positionPlayer1);
-                Console.WriteLine("Position du joueur 2: {0}", positionPlayer2);
+                Move(listOfPlayers, positionOfPlayers);
+                for (int i = 0; i < positionOfPlayers.Length; i++)
+                {
+                    Console.WriteLine("{0} a avancé(e) de {1} case(s)", listOfPlayers[i], positionOfPlayers[i]);
+                }
+                play = IsFinish(positionEnd, positionOfPlayers, listOfPlayers);
+                FallInTrap(positionOfPlayers, listOfPlayers);
+                Bonus(positionOfPlayers, listOfPlayers);
             }
 
         }
@@ -53,102 +40,125 @@
             int resultat = rand.Next(1, 7);
             return resultat;
         }
-        public static bool IsFinish(int positionPlayer1, int positionPlayer2, int positionEnd)
+        
+        public static int[] Move(List<string> listOfPlayers, int[] moveOfPlayers)
         {
-            if (positionPlayer1 == positionEnd)
+            int i = 0;
+            foreach (var player in listOfPlayers)
             {
-                Console.WriteLine("Victoire pour le joueur 1");
-                Console.WriteLine("Fin de partie");
-                return false;
-            } else if (positionPlayer2 == positionEnd)
-            {
-                Console.WriteLine("Victoire pour le joueur 2");
-                Console.WriteLine("Fin de partie");
+                int move = LancerDeDes();
+                moveOfPlayers[i] += move;
+                i++;
             }
+
+            return moveOfPlayers;
+        }
+
+        public static bool IsFinish(int positionEnd, int[] positionOfPlayers, List<string> listOfPlayers)
+        {
+            bool victoire =false;
+            for (int i = 0; i < listOfPlayers.Count; i++)
+            {
+                if (positionOfPlayers[i] == positionEnd)
+                {
+                    Console.WriteLine("Victoire pour le joueur {0}", listOfPlayers[i]);
+                    victoire = true;
+                    break;
+                    
+                }
+            }
+            if(victoire)
+                return false;
             return true;
         }
 
-        public static int FallInTrap(int positionPlayer)
+        public static int[] FallInTrap(int[] positionOfPlayers, List<string> listOfPlayers)
         {
-            if (positionPlayer == 37)
+            for (int i = 0; i < listOfPlayers.Count; i++)
             {
-                Console.WriteLine("Piège, vous retombez case 12");
-                return 12;
+                switch (positionOfPlayers[i])
+                {
+                    case 37:
+                        Console.WriteLine("Dommage {0}, vous tombez dans un piège. Retour à la case 12", listOfPlayers[i]);
+                        positionOfPlayers[i] = 12;
+                        break;
+                    case 14:
+                        Console.WriteLine("Dommage {0}, vous tombez dans un piège. Retour à la case 7", listOfPlayers[i]);
+                        positionOfPlayers[i] = 7;
+                        break;
+                    case 46:
+                        Console.WriteLine("Dommage {0}, vous tombez dans un piège. Retour à la case 33", listOfPlayers[i]);
+                        positionOfPlayers[i] = 33;
+                        break;
+                    case >50:
+                        Console.WriteLine("Dommage {0}, vous êtes allez trop loin! Vous retombez à la case 25", listOfPlayers[i]);
+                        positionOfPlayers[i] = 25;
+                        break;
+                    default:
+                        break;
+                }
             }
-            if (positionPlayer == 14)
-            {
-                Console.WriteLine("Piège, vous retombez case 7");
-                return 7;
-            }
-            if (positionPlayer == 46)
-            {
-                Console.WriteLine("Piège, vous retombez case 33");
-                return 33;
-            }
-
-            if (positionPlayer > 50)
-            {
-                Console.WriteLine("Dommage, vous êtes allez trop loin, vous retombés case 25");
-                return 25;
-            }
-            return positionPlayer;
+            return positionOfPlayers;
         }
-        public static int Bonus(int PositionPlayer)
+        public static int[] Bonus(int[] positionOfPlayers, List<string> listOfPlayers)
         {
-            if (PositionPlayer == 20)
+            for (int i = 0; i < listOfPlayers.Count; i++)
             {
-                Console.WriteLine("Bonus ! Vous allez directement case 35");
-                return 35;
+                switch (positionOfPlayers[i])
+                {
+                    case 20:
+                        Console.WriteLine("Bonus pour {0}! Vous allez directment à la case 35", listOfPlayers[i]);
+                        positionOfPlayers[i] = 35;
+                        break;
+                    case 2:
+                        Console.WriteLine("Bonus pour {0}! Vous allez directment à la case 17", listOfPlayers[i]);
+                        positionOfPlayers[i] = 17;
+                        break;
+                    case 31:
+                        Console.WriteLine("Bonus pour {0}! Vous allez directment à la case 43", listOfPlayers[i]);
+                        positionOfPlayers[i] = 43;
+                        break;
+                    default:
+                        break;
+                }
             }
-            if (PositionPlayer == 2)
-            {
-                Console.WriteLine("Bonus ! Vous allez directement case 17");
-                return 17;
-            }
-            if (PositionPlayer == 31)
-            {
-                Console.WriteLine("Bonus ! Vous allez directement case 43");
-                return 35;
-            }
-            return PositionPlayer;
+            return positionOfPlayers;
         }
 
-        public static void Display(int positionPlayer1, int positionPlayer2, char player1, char player2)
-        {
-            char[] map = new char[50];
-            Console.Write("Carte joueur 1 ");
-            for (int j = 0; j < map.Length; j++)
-            {
+        //public static void Display(int positionPlayer1, int positionPlayer2, char player1, char player2)
+        //{
+        //    char[] map = new char[50];
+        //    Console.Write("Carte joueur 1 ");
+        //    for (int j = 0; j < map.Length; j++)
+        //    {
 
-                Console.Write(' ');
-                if (positionPlayer1 == j)
-                {
-                    Console.Write(player1);
-                }
-                else
-                {
-                    Console.Write('-');
-                }
-                Console.Write(' ');
-            }
-            Console.WriteLine();
-            Console.Write("Carte joueur 2 ");
-            for (int j = 0; j < map.Length; j++)
-            {
-                Console.Write(' ');
-                if (positionPlayer2 == j)
-                {
-                    Console.Write(player2);
-                }
-                else
-                {
-                    Console.Write('-');
-                }
-                Console.Write(' ');
-            }
-            Console.WriteLine();
-
-
-        }
+        //        Console.Write(' ');
+        //        if (positionPlayer1 == j)
+        //        {
+        //            Console.Write(player1);
+        //        }
+        //        else
+        //        {
+        //            Console.Write('-');
+        //        }
+        //        Console.Write(' ');
+        //    }
+        //    Console.WriteLine();
+        //    Console.Write("Carte joueur 2 ");
+        //    for (int j = 0; j < map.Length; j++)
+        //    {
+        //        Console.Write(' ');
+        //        if (positionPlayer2 == j)
+        //        {
+        //            Console.Write(player2);
+        //        }
+        //        else
+        //        {
+        //            Console.Write('-');
+        //        }
+        //        Console.Write(' ');
+        //    }
+        //    Console.WriteLine();
+        //}
     }
 }
